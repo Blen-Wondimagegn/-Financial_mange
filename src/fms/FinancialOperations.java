3d763a14911d7229fb87ba071b1019ebe9e048ce
Blen-Wondimagen <mekuriablen@example.com>
2024-08-02 10:39:02 -0700

    FinancialOperations class

--- src/fms/FinancialOperations.java
+++ src/fms/FinancialOperations.java
@@ -1,15 +1,87 @@
 package fms;
 
 import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
 
 public class FinancialOperations {
+//    public static double getHourlyRateSum(List<Employee> employees){
+//        double sum = 0.0;
+//
+//        for (Employee employee : employees) {
+//            sum += employee.getHourly_rate();
+//        }
+//
+//        return sum;
+//    }
+
+    /*return employees.stream()
+.mapToDouble(Employee :: getHourly_rate)
+        .sum();
+     */
     public static double getHourlyRateSum(List<Employee> employees){
-        double sum = 0.0;
 
-        for (Employee employee : employees) {
-            sum += employee.getHourlyRate();
-        }
+        return employees.stream()
+                .mapToDouble(Employee :: getHourly_rate)
+                .sum();
+    }
 
-        return sum;
+    public static double sumDepartmentSalary(List<Employee> employees, String department){
+        return employees.stream()
+                .filter(e -> e.getDepartment().equals(department))
+                .mapToDouble(Employee :: getSalary)
+                .sum();
+    }
+    public static double getAverageSalaryByDepartment(List<Employee> employees, String department){
+        return employees.stream()
+                .filter(e -> e.getDepartment().equals(department))
+                .mapToDouble(Employee::getSalary)
+                .average()
+                .orElse(0.0);
+    }
+
+    public static double sumDepartmentHourlyRate(List<Employee> employees, String department){
+        return employees.stream()
+                .filter(e -> e.getDepartment().equals(department))
+                .mapToDouble(Employee::getHourly_rate)
+                .sum();
+    }
+
+    public static double getAverageHourlyRateByDepartment(List<Employee> employees, String department){
+        return employees.stream()
+                .filter(e -> e.getDepartment().equals(department))
+                .mapToDouble(Employee::getHourly_rate)
+                .average()
+                .orElse(0.0);
+    }
+    public static Map<String, Double> getTotalSalaryByDepartment(List<Employee> employees){
+        return employees.stream()
+                .collect(Collectors.groupingBy(
+                        Employee::getDepartment,
+                        Collectors.summingDouble(Employee::getSalary)
+                ));
+    }
+
+    public static Map<String, Double> getAverageSalaryByDepartment(List<Employee> employees){
+        return employees.stream()
+                .collect(Collectors.groupingBy(
+                        Employee::getDepartment,
+                        Collectors.averagingDouble(Employee::getSalary)
+                ));
+    }
+
+    public static Map<String, Double> getTotalHourlyRateByDepartment(List<Employee> employees){
+        return employees.stream()
+                .collect(Collectors.groupingBy(
+                        Employee::getDepartment,
+                        Collectors.summingDouble(Employee::getHourly_rate)
+                ));
+    }
+    public static Map<String, Double> getAverageHourlyRateByDepartment(List<Employee> employees){
+        return employees.stream()
+                .collect(Collectors.groupingBy(
+                        Employee::getDepartment,
+                        Collectors.averagingDouble(Employee::getHourly_rate)
+                ));
     }
 }
--- src/fms/Main.java
+++ src/fms/Main.java
@@ -2,6 +2,8 @@ package fms;
 
 import java.util.List;
 
+import static fms.FinancialOperations.*;
+
 public class Main {
     public static void main(String[] args) {
 
@@ -10,6 +12,24 @@ public class Main {
         List<Employee> employees = CSVReader2.readEmployeeFromCsv("file.csv");
         System.out.println(employees);
         CSVWriter.writeEmployeesToCSV(employees, "output_employees.csv");
+        System.out.println("***********getHourlyRateSum**********");
+        System.out.println(getHourlyRateSum(employees));
+        System.out.println("***********sum DepartmentSalary**********");
+        System.out.println(sumDepartmentSalary(employees, "Finance"));
+        System.out.println("***********getAverageSalaryByDepartment**********");
+        System.out.println(getAverageSalaryByDepartment(employees, "Finance"));
+        System.out.println("***********sumDepartmentHourlyRate**********");
+        System.out.println(sumDepartmentHourlyRate(employees, "Finance"));
+        System.out.println("***********getAverageHourlyRateByDepartment**********");
+        System.out.println(getAverageHourlyRateByDepartment(employees, "Finance"));
+        System.out.println("***********getTotalSalaryByDepartment**********");
+        System.out.println(getTotalSalaryByDepartment(employees));
+        System.out.println("***********getAverageSalaryByDepartment**********");
+        System.out.println(getAverageSalaryByDepartment(employees));
+        System.out.println("***********getAverageHourlyRateByDepartment**********");
+        System.out.println(getAverageHourlyRateByDepartment(employees));
+
+
 
     }catch(IllegalArgumentException exception)
     {
